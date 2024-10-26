7e1f84591d8071cb4043000b2c2aa46f424c6abd
sajna <sajnashuhaib38@gmail.com>
2024-10-26 22:11:31 +0530

    commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Sign In',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue.shade800,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Welcome back! Please enter your credentials to login',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 30),
+
+          // Username Field
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5), // Slight shadow color
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(0, 2), // Shadow position
+                ),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'User Name',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.grey)),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade200,
+                    width: 1.0,
+                  ),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          // Password Field
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5), // Slight shadow color
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(0, 2), // Shadow position
+                ),
+              ],
+            ),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade200,
+                    width: 1.0,
+                  ),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {
+                // Add forgot password logic
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot your ',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    "Password?",
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 100),
+              backgroundColor: const Color.fromARGB(255, 11, 58, 97),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: Text(
+              'Sign In',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          // Sign Up Section
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Don't have an account?",
+                style: TextStyle(color: Colors.white70),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Add sign-up logic
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
